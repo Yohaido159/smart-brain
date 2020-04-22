b319aa90c4612980ddbd2f4306aea8dde69ba6b8
Yohai <yohaido159@gmail.com>
2020-04-22 08:51:47 +0300

    d

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: "",
+    };
+  }
+
+  componentDidMount() {
+    this.genNumber(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.name
+    ) {
+      return null;
+    }
+
+    this.genNumber(this.props.entries);
+  }
+
+  genNumber = (entries) => {
+    fetch(
+      `https://i07fnm0f93.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ num: data.input }))
+      .catch(console.log("err"));
+  };
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f3">{`Rank is: "${this.state.num}"`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
